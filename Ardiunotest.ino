76acc1dbfadb9b62f49afcccb6945c0a44b1ee06
EnesUCA <79279212+EnesUCA@users.noreply.github.com>
2021-06-10 23:57:23 +0300

    Led girislerinin ve degiskenlerin tanimi

--- Ardiunotest.ino
+++ Ardiunotest.ino
@@ -1,6 +1,10 @@
 #include "Ardiuno.h"
 #include <SoftwareSerial.h> \\Bluetooth giris cikis tanımı
 SoftwareSerial mySerial(4, 6); //HC-06'nın Rx , Tx bacakları tanımlandı
+int kirmizi = 8;
+int sari = 9;
+int yesil = 10;
+int onay ;
 void setup() {
   // put your setup code here, to run once:
 
